f83f73cb3ce891bc813175110ebd6f3163ecac70
timothe laude <timothelaude@MacBook-Pro-de-timothe.local>
2019-10-22 11:49:54 +0200

    start eval

---
+++ src/main/java/co/simplon/evaluation/EvalSpringApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalSpringApplication.class, args);
+	}
+
+}
