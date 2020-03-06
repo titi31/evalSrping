15dcf4e3760eab8798eb61ba02ab1b0ceea96716
Mr.Robot <classic.graal095@gmail.com>
2020-03-06 19:21:46 +0100

    indentation

--- src/main/java/co/simplon/evaluation/entities/City.java
+++ src/main/java/co/simplon/evaluation/entities/City.java
@@ -4,20 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Collection;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class City implements Serializable {
+public class City implements Serializable 
+{
+
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
+    
     @OneToMany(mappedBy = "city")
     private Collection<Hotel> hotel;
+    
 }
