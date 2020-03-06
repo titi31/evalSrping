e16b74a7e5ebe51f9ed9d1f57eb01d8ce66b2469
Mr.Robot <classic.graal095@gmail.com>
2020-03-06 19:17:05 +0100

    indentation

--- src/main/java/co/simplon/evaluation/service/AccountService.java
+++ src/main/java/co/simplon/evaluation/service/AccountService.java
@@ -1,7 +1,10 @@
 package co.simplon.evaluation.service;
+
 import co.simplon.evaluation.entities.AppRole;
 import co.simplon.evaluation.entities.AppUser;
-public interface AccountService {
+
+public interface AccountService 
+{
     public AppUser saveUser(String username,String password,String confirmedPassword);
     public AppRole save(AppRole role);
     public AppUser loadUserByUsername(String username);
