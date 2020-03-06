dc3a3428987b0239d7219dd49cdfb6e3f838f8e0
Mr.Robot <classic.graal095@gmail.com>
2020-03-06 19:22:32 +0100

    indentation

--- src/main/java/co/simplon/evaluation/entities/AppUser.java
+++ src/main/java/co/simplon/evaluation/entities/AppUser.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -15,15 +14,22 @@ import java.util.Collection;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class AppUser {
+public class AppUser 
+{
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     @Column(unique = true)
     private String username;
+    
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+    
     private boolean actived;
+    
     @ManyToMany(fetch = FetchType.EAGER)
     private Collection<AppRole> roles = new ArrayList<>();
+    
 }
