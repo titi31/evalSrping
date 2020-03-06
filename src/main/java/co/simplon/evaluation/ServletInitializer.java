0b9c7387c2f4428f4de39678c2038b2830be60aa
Mr.Robot <classic.graal095@gmail.com>
2020-03-06 19:13:39 +0100

    indentation

--- src/main/java/co/simplon/evaluation/ServletInitializer.java
+++ src/main/java/co/simplon/evaluation/ServletInitializer.java
@@ -1,11 +1,17 @@
 package co.simplon.evaluation;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-public class ServletInitializer extends SpringBootServletInitializer{
+
+public class ServletInitializer extends SpringBootServletInitializer
+{
+
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) 
+    {
         return application.sources(EvalSpringApplication.class);
     }
+    
 }
 
 
